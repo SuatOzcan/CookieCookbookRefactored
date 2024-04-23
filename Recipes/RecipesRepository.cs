cc144cd2f823cd62a89a88aa3be6c83962cddc68
SuatOzcan <41187131+SuatOzcan@users.noreply.github.com>
2024-04-23 16:53:01 +0300

    Refactor loops in RecipesRepository to LINQ queries.

--- Recipes/RecipesRepository.cs
+++ Recipes/RecipesRepository.cs
@@ -19,45 +19,64 @@ public class RecipesRepository : IRecipesRepository
 
     public List<Recipe> Read(string filePath)
     {
-        List<string> recipesFromFile = _stringsRepository.Read(filePath);
-        var recipes = new List<Recipe>();
+        List<Recipe> recipes = _stringsRepository.Read(filePath)
+                                                 .Select(recipeFromFile => RecipeFromString(recipeFromFile))
+                                                 .ToList();
+        
+        //var recipes = new List<Recipe>();
 
-        foreach (var recipeFromFile in recipesFromFile)
-        {
-            var recipe = RecipeFromString(recipeFromFile);
-            recipes.Add(recipe);
-        }
+        //foreach (var recipeFromFile in recipesFromFile)
+        //{
+        //    var recipe = RecipeFromString(recipeFromFile);
+        //    recipes.Add(recipe);
+        //}
 
         return recipes;
     }
 
     private Recipe RecipeFromString(string recipeFromFile)
     {
-        var textualIds = recipeFromFile.Split(Separator);
-        var ingredients = new List<Ingredient>();
-
-        foreach (var textualId in textualIds)
-        {
-            var id = int.Parse(textualId);
-            var ingredient = _ingredientsRegister.GetById(id);
-            ingredients.Add(ingredient);
-        }
-
+        var ingredients = recipeFromFile.Split(Separator)
+                                        .Select(textualId => int.Parse(textualId))
+                                        .Select(id => _ingredientsRegister.GetById(id));
         return new Recipe(ingredients);
+
+        //var ingredients = new List<Ingredient>();
+
+        //foreach (var textualId in textualIds)
+        //{
+        //    var id = int.Parse(textualId);
+        //    var ingredient = _ingredientsRegister.GetById(id);
+        //    ingredients.Add(ingredient);
+        //}
+
+        //return new Recipe(ingredients);
     }
 
     public void Write(string filePath, List<Recipe> allRecipes)
     {
-        var recipesAsStrings = new List<string>();
-        foreach (var recipe in allRecipes)
-        {
-            var allIds = new List<int>();
-            foreach (var ingredient in recipe.Ingredients)
-            {
-                allIds.Add(ingredient.Id);
-            }
-            recipesAsStrings.Add(string.Join(Separator, allIds));
-        }
+
+        //var recipesAsStrings = allRecipes.Select(recipe =>
+        //                                                     {
+        //                                                         var allIds = recipe.Ingredients
+        //                                                                     .Select(ingredient => ingredient.Id);
+        //                                                         return string.Join(Separator, allIds);
+        //                                                      }).ToList();
+
+        var recipesAsStrings = allRecipes.Select(recipe => string.Join(Separator, recipe.Ingredients.
+                                                                        Select(ingredient => ingredient.Id)))
+                                         .ToList();
+        
+        //var recipesAsStrings = new List<string>();
+        //foreach (var recipe in allRecipes)
+        //{
+        //    var allIds = recipe.Ingredients.Select(ingredient => ingredient.Id);
+        //    //foreach (var ingredient in recipe.Ingredients)
+        //    //{
+        //    //    allIds.Add(ingredient.Id);
+        //    //}
+        //    recipesAsStrings.Add(string.Join(Separator, allIds));
+        //}
 
         _stringsRepository.Write(filePath, recipesAsStrings);
     }
